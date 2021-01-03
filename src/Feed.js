0acd3432ef07035885e2e6c85b8ffe47dbd58863
King Ameen <ameenilyas78.com>
2021-01-03 14:41:12 +0500

    twitter App

---
+++ src/Feed.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import "./Feed.css";
+import TweetBox from "./TweetBox";
+import Post from "./Post";
+import FlipMove from "react-flip-move";
+import { useStateValue } from "./StateProvider";
+
+function Feed() {
+  const [{ posts }, dispatch] = useStateValue();
+  console.log(posts);
+
+  return (
+    <div className="feed">
+      <div className="feed__header">
+        <h2>Home</h2>
+      </div>
+      <TweetBox />
+      <FlipMove>
+        {posts.map((post, i) => (
+          <Post
+            key={i}
+            displayName={post.displayName}
+            Username={post.Username}
+            verified={post.verified}
+            text={post.text}
+            image={post.image}
+            avatar={post.avatar}
+          />
+        ))}
+      </FlipMove>
+    </div>
+  );
+}
+
+export default Feed;
---
+++ src/StateProvider.js
@@ -0,0 +1,11 @@
+import React, { createContext, useContext, useReducer } from "react";
+
+const StateContext = createContext();
+
+export const StateProvider = ({ initialization, reducer, children }) => (
+  <StateContext.Provider value={useReducer(reducer, initialization)}>
+    {children}
+  </StateContext.Provider>
+);
+
+export const useStateValue = () => useContext(StateContext);
